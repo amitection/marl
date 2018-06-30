18930a8d95f0d3758d1cc5c48b5c56b952b5a909
Amit Prasad <amitprasad53121@gmail.com>
2018-06-30 17:34:01 +0100

    batch learning. batch size = 48

--- marlagent/agent/dqn/dqn.py
+++ marlagent/agent/dqn/dqn.py
@@ -5,9 +5,9 @@ import torch
 import torch.autograd as autograd
 import torch.optim as optim
 
-from feat_extractor import FeatureExtractor
 from marlagent import rlagent
 from marlagent.agent.dqn.model import DQN
+from marlagent.agent.dqn.replay_buffer import ReplayBuffer
 
 USE_CUDA = torch.cuda.is_available()
 dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
@@ -30,18 +30,19 @@ optimizer_spec = OptimizerSpec(
 
 class DQNAgent(rlagent.RLAgent):
 
-    def __init__(self, n_features=69):
+    def __init__(self):
 
         super(DQNAgent, self).__init__()
         print("DQN initiated...")
 
-        self.num_param_updates = 0
-        self.curr_batch_size = 0
+        self.learning_freq = 48
 
         self.n_features = self.feat_extractor.get_n_features()
 
         # Instantiating a MLP model
         self.Q = DQN(self.n_features)
+
+        self.replay_buffer = ReplayBuffer(size = 48, n_features = self.n_features)
 
 
         # Construct Q network optimizer function
@@ -67,34 +68,56 @@ class DQNAgent(rlagent.RLAgent):
 
 
     def update(self, state, action, next_state, reward):
+
         features = self.feat_extractor.get_features(state, action)
-        feat_arr = self.__transform_to_numpy(features)
 
-        q_value_next_state = (self.discount * self.compute_value_from_qValues(next_state))
-        q_value_curr_state = self.get_qValue(state, action)
+        # store the converted state in the replay buffer
+        self.replay_buffer.store_transition(features, action, reward)
 
-        # Compute Bellman error
-        bellman_error = reward + q_value_next_state - q_value_curr_state
+        # extract the current index of the replay buffer
+        # sub by -1 as the index is incremented after each insertion
+        curr_idx = self.replay_buffer.idx - 1
 
-        # clip the bellman error between [-1 , 1]
-        clipped_bellman_error = bellman_error.clamp(-1, 1)
-        print("Bellman Error:", clipped_bellman_error)
+        # Perform the update in a batch. Apply the average error over all fields
+        if(curr_idx % self.learning_freq == 0):
 
-        d_error = clipped_bellman_error * -1.0
-        print("Delta Error:",d_error)
+            obs, next_obs, reward = self.replay_buffer.sample(batch_size=48)
 
-        self.write_to_file(data=d_error, path_to_file='assets/' + state.name + 'error.csv')
+            obs_batch = Variable(torch.from_numpy(obs).type(dtype))
+            reward_batch = Variable(torch.from_numpy(reward))
+            next_obs_batch = Variable(torch.from_numpy(next_obs).type(dtype))
 
-        # Clear previous gradients before backward pass
-        self.optimizer.zero_grad()
+            current_Q_values = self.Q(obs_batch)
+            target_Q_values = self.Q(next_obs_batch).detach()
 
-        new_q_value_curr_state = Variable(q_value_curr_state.data, requires_grad=True)
-        new_q_value_curr_state.backward()
-        # q_value_curr_state.backward(d_error.data.unsqueeze(1))
+            q_value_curr_state = current_Q_values
+            q_value_next_state = reward_batch + (self.discount * target_Q_values)
 
-        # Perfom the update
-        self.optimizer.step()
+            # Compute Bellman error
+            bellman_error = reward + q_value_next_state - q_value_curr_state
 
+            # clip the bellman error between [-1 , 1]
+            clipped_bellman_error = bellman_error.clamp(-1, 1)
+            print("Bellman Error:", clipped_bellman_error)
+
+            d_error = clipped_bellman_error * -1.0
+            print("Delta Error:",d_error)
+
+            self.write_to_file(data=d_error, path_to_file='assets/' + state.name + 'error.csv')
+
+            # Clear previous gradients before backward pass
+            self.optimizer.zero_grad()
+
+            new_q_value_curr_state = Variable(q_value_curr_state.data, requires_grad=True)
+            # new_q_value_curr_state.backward()
+            # new_q_value_curr_state.backward(d_error.data.unsqueeze(1))
+            new_q_value_curr_state.backward(d_error.data.unsqueeze(1))
+
+            # Perfom the update
+            self.optimizer.step()
+
+            # Clear stored values in the replay buffer
+            self.replay_buffer.reset()
 
     def __transform_to_numpy(self, features):
         numpy_arr = np.array(features, dtype=np.float32)
