0c431ecd034831083f034f2ce0994968dddda381
Amit Prasad <amitprasad53121@gmail.com>
2018-07-31 19:18:14 +0100

    added summer schedules

--- nameserver.py
+++ nameserver.py
@@ -89,7 +89,8 @@ class NameServer:
 
         try:
             # for timestep in range(0, 1200, 30):
-            for timestep in range(7200, 11490, 30):
+            # for timestep in range(7200, 11490, 30): # winter
+            for timestep in range(218880, 223170, 30):  # summer
 
                 for name in agent_name_arr:
                     d = d_map[name]
