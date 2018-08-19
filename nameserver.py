d7a188332d50b0eabd4a082cb5e2bfbbfd2c68c1
Amit Prasad <amitprasad53121@gmail.com>
2018-08-19 11:44:44 +0100

    added exception handler

--- nameserver.py
+++ nameserver.py
@@ -11,7 +11,7 @@ class NameServer:
 
         self.ns = ns
         self.agentname = agentname
-        time.sleep(5) # to let all the agents load due to startup latency
+        time.sleep(3) # to let all the agents load due to startup latency
 
 
     def schedule_job(self, server_agent):
@@ -19,14 +19,26 @@ class NameServer:
         self.d2 = self._load_data("assets/house2_consumption.csv")
         self.d3 = self._load_data("assets/house3_consumption.csv")
 
-        d_map ={}
-        for i in range(1, 10, 3):
-            d_map['A' + str(i)] = self.d1
-            d_map['A' + str(i+1)] = self.d2
-            d_map['A' + str(i+2)] = self.d3
+        # d_map ={}
+        # for i in range(1, 10, 3):
+        #     d_map['A' + str(i)] = self.d1
+        #     d_map['A' + str(i+1)] = self.d2
+        #     d_map['A' + str(i+2)] = self.d3
+        #
+        # d_map['A10'] = self.d1
 
-        d_map['A10'] = self.d1
-
+        d_map = {
+            "A1": self.d1,
+            "A2": self.d2,
+            "A3": self.d3,
+            "A4": self.d1,
+            "A5": self.d2,
+            "A6": self.d3,
+            "A7": self.d1,
+            "A8": self.d2,
+            "A9": self.d3,
+            "A10": self.d1,
+        }
         # extracting the list of agents
         agents = self.ns.agents()
         server_agent.log_info("Registering client details...")
@@ -44,37 +56,43 @@ class NameServer:
 
         max_iter = 500
 
-        for iter in range(max_iter):
-            message['iter'] = iter
+        try:
+            server_agent.log_info("Starting distribution of data...")
+            
+            for iter in range(max_iter):
+                message['iter'] = iter
 
-            server_agent("List of Agents,......... ",agent_name_arr)
-            last_message = self.dispatch_energy_data(server_agent, message, agent_name_arr, agent_addr, d_map)
-            server_agent.log_info("Iteration (%s) complete!"%iter)
+                server_agent.log_info("List of Agents,......... ",agent_name_arr)
+                last_message = self.dispatch_energy_data(server_agent, message, agent_name_arr, agent_addr, d_map)
+                server_agent.log_info("Iteration (%s) complete!"%iter)
 
-            if iter <= (max_iter-11):
-                eoi_message = {
-                    'topic': 'END_OF_ITERATION',
-                    'iter': iter,
-                    'time': last_message['time']
-                }
-            else:
-                # last iteration will warn the agents to exploit their policies completely
-                eoi_message = {
-                    'topic': 'TRAINING_COMPLETE',
-                    'iter': iter,
-                    'time': last_message['time']
-                }
+                if iter <= (max_iter-11):
+                    eoi_message = {
+                        'topic': 'END_OF_ITERATION',
+                        'iter': iter,
+                        'time': last_message['time']
+                    }
+                else:
+                    # last iteration will warn the agents to exploit their policies completely
+                    eoi_message = {
+                        'topic': 'TRAINING_COMPLETE',
+                        'iter': iter,
+                        'time': last_message['time']
+                    }
 
-            time.sleep(5)
-            # EOI: notify each agent to save its status at the end of each iteration
+                time.sleep(5)
+                # EOI: notify each agent to save its status at the end of each iteration
+                for name in agent_name_arr:
+                    self._send_message(server_agent, agent_addr[name], alias='consumption', message=eoi_message)
+                time.sleep(5)
+
+            # Exit Message after iterations done
+            # Safe shutdown of all agents for testing
             for name in agent_name_arr:
-                self._send_message(server_agent, agent_addr[name], alias='consumption', message=eoi_message)
-            time.sleep(5)
+                self._send_message(server_agent, agent_addr[name], alias='consumption', message={'topic': 'exit'})
 
-        # Exit Message after iterations done
-        # Safe shutdown of all agents for testing
-        for name in agent_name_arr:
-            self._send_message(server_agent, agent_addr[name], alias='consumption', message={'topic': 'exit'})
+        except Exception:
+            print(traceback.format_exc())
 
 
     def _load_data(self, path_to_file):
